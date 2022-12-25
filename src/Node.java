6a3703e915c3e9cac7d42fc7cf8c6a1c8a3b9f85
maork <kovlermaor@gmail.com>
2022-12-25 18:42:38 +0200

    Documentation for Node class.

--- src/Node.java
+++ src/Node.java
@@ -1,15 +1,30 @@
 import java.util.ArrayList;
 
+/**
+ * This is the Node class which represents a single router in our net work.
+ */
 public class Node extends Thread {
-    private int id;
-    private ArrayList<Pair<Integer,int[]>> neighbors;
+    private int id; //The id of the router
+    private ArrayList<Pair<Integer,int[]>> neighbors; /*A list of pairs which contains all the neighbors of the
+    node */
+    private int[][] adj_matrix; //This is the matrix for the link state routing algorithm
 
-    private int[][] adj_matrix;
-
+    /**
+     * This function returns the node's id
+     * @return id
+     */
     int get_node_id() {return this.id;}
 
+    /**
+     * This function returns the node's neighbors list
+     * @return neighbors
+     */
     ArrayList<Pair<Integer, int[]>> get_neighbors() {return this.neighbors;}
 
+    /**
+     * This function returns the node's matrix
+     * @return adj_matrix
+     */
     int[][] get_adj_matrix() {return this.adj_matrix;}
 
 //    void Node
