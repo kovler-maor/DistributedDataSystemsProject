367eeb8df841fbe3cb37e23e28a5a7eb1c07e80a
maork <kovlermaor@gmail.com>
2022-12-27 18:15:17 +0200

    This version is after finishing writhing the read_text method in ExManager

--- src/ExManager.java
+++ src/ExManager.java
@@ -1,17 +1,17 @@
-import java.util.ArrayList;
-
+import java.util.*;
+import java.io.*;
+import java.net.*;
 public class ExManager {
     private String path;
-    private int num_of_nodes;
     private ArrayList<Node> list_of_nodes;
     // your code here
 
-    public ExManager(String path){
+    public ExManager(String path) {
         this.path = path;
         // your code here
     }
 
-    public Node get_node(int id){
+    public Node get_node(int id) {
         // your code here
     }
 
@@ -19,15 +19,75 @@ public class ExManager {
         return this.num_of_nodes;
     }
 
-    public void update_edge(int id1, int id2, double weight){
+    public void update_edge(int id1, int id2, double weight) {
         //your code here
     }
 
-    public void read_txt(){
+    public void read_txt() throws IOException {
+        /**
+         * This function reads from file and creates the nodes and their neighbors.
+         */
+        FileReader fr = new FileReader(this.path);
+        BufferedReader br = new BufferedReader(fr);
+        String line;
+        Node node = null;
+        while ((line = br.readLine()) != "stop") {
+            String[] words = line.split("\\s+");
+            ArrayList<Integer> nodes_ids = get_nodes_ids();
+            if (!nodes_ids.contains(Integer.parseInt(words[0]))) {
+                node = new Node(Integer.parseInt(words[0]));
+            } else {
+                node = get_node(Integer.parseInt(words[0]));
+            }
+            for (int i = 1; i < words.length; i += 4) {
+                int neighbor_id = Integer.parseInt(words[i]);
+                int neighbor_weight = Integer.parseInt(words[i + 1]);
+                int neighbor_send_port = Integer.parseInt(words[i + 2]);
+                int neighbor_listen_port = Integer.parseInt(words[i + 3]);
+
+                node.add_neighbor(neighbor_id, neighbor_weight, neighbor_send_port, neighbor_listen_port);
+            }
+        }
+    }
+
+    public void start() {
         // your code here
     }
 
-    public void start(){
-        // your code here
+    private ArrayList<Integer> get_nodes_ids() {
+        /**
+         * This function takes the list_of_nodes attribute of the ExManager class and
+         * returns an ArrayList of all the unique IDs of those Nodes.
+         * @return ids_list_of_nodes
+         */
+        ArrayList<Integer> ids_list_of_nodes = null;
+        for (Node node : this.list_of_nodes) {
+            ids_list_of_nodes.add(node.get_node_id());
+        }
+        return ids_list_of_nodes;
     }
+
+    private Node get_node(Integer id) {
+        /**
+         * This function takes the list_of_nodes attribute of the ExManager class and
+         * returns a node with the unique ID that been given.
+         * @int id -> node id
+         * @return node -> an already existing node with the given ID.
+         */
+        try {
+            Node wanted_node = null;
+            for (Node node : this.list_of_nodes) {
+                if (id.intValue() == node.get_node_id()) {
+                    wanted_node = node;
+                    break;
+                }
+            }
+            return wanted_node;
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
 }
--- src/Node.java
+++ src/Node.java
@@ -1,33 +1,54 @@
-import java.util.ArrayList;
+import java.util.*;
+import java.io.*;
+import java.net.*;
 
 /**
  * This is the Node class which represents a single router in our net work.
  */
 public class Node extends Thread {
-    private int id; //The id of the router
-    private ArrayList<Pair<Integer,int[]>> neighbors; /*A list of pairs which contains all the neighbors of the
-    node */
-    private int[][] adj_matrix; //This is the matrix for the link state routing algorithm
+    private int id; // The id of the router.
+    private ArrayList<Pair<Integer,int[]>> neighbors; // A list of pairs which contains all the neighbors of the node.
+    private int[][] adj_matrix; //This is the matrix for the link state routing algorithm.
 
-    /**
-     * This function returns the node's id
-     * @return id
-     */
-    int get_node_id() {return this.id;}
+    public Node(int id){
+        this.id = id;
+    }
 
-    /**
-     * This function returns the node's neighbors list
-     * @return neighbors
-     */
-    ArrayList<Pair<Integer, int[]>> get_neighbors() {return this.neighbors;}
+    public void add_neighbor(int neighbor_id, int neighbor_weight, int neighbor_send_port, int neighbor_listen_port){
+        /**
+         * This function adds a new neighbor to the node's neighbors list.
+         * @int neighbor_id -> the neighbor's id
+         * @int neighbor_weight the -> the edge's weight
+         * @int neighbor_send_port -> the sending port
+         * @int neighbor_listen_port -> the receiving port
+         */
+        Integer key = neighbor_id;
+        int[] value = new int[] {neighbor_weight, neighbor_send_port, neighbor_listen_port};
+        Pair pair = new Pair<>(key,value);
+        this.neighbors.add(pair);
+    }
+    int get_node_id() {
+        /**
+         * This function returns the node's id
+         * @return id
+         */
+        return this.id;
+    }
 
-    /**
-     * This function returns the node's matrix
-     * @return adj_matrix
-     */
-    int[][] get_adj_matrix() {return this.adj_matrix;}
+    ArrayList<Pair<Integer, int[]>> get_neighbors() {
+        /**
+         * This function returns the node's neighbors list
+         * @return neighbors
+         */
+        return this.neighbors;
+    }
 
-//    void Node
-
+    int[][] get_adj_matrix() {
+        /**
+         * This function returns the node's matrix
+         * @return adj_matrix
+         */
+        return this.adj_matrix;
+    }
 
 }
